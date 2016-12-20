c6d2fcc3265e381954f15154a0aa5c286e0aa75d
Daniel Hirunrusme <daniel@halfhalf.us>
2016-12-20 00:56:54 -0500

    init setup of the page

---
+++ src/js/modules/video.js
@@ -0,0 +1,11 @@
+var settings = require( "modules/settings" ),
+	throttle = require("modules/throttle");
+	
+
+module.exports = function( el ) {
+		var $el = $( el ),
+		$window = $( window );
+
+
+};
+  
